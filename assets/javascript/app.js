3454582398917220c87ef95518af083346237ea9
christina <chrstina.sosa024@gmail.com>
2019-09-15 17:55:41 -0700

    questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,93 @@
+const myQuestions = [
+    {
+        questions: "To get over Ricard, what did Monica start Making?",
+        answers: {
+            a: "Marmalade",
+            b: "Jam",
+            c: "Pancakes",
+            d: "Eggs",
+        },
+        correctAnswer: "B"
+    },
+
+    {
+        questions: "Monica couldn't tell time until she was what age?",
+        answers: {
+            a:"13",
+            b: "14",
+            c: "12",
+            d: "15",
+        },
+        correctAnswer: "13"
+    },
+    {
+        questions: "What does Ross call his Thanksgiving leftover sandwhich?",
+        answers: {
+            a: "The Drool Maker",
+            b: "Bread Heaven",
+            c: "The Moist Maker",
+            d: "Temptation",
+        },
+        correctAnswer: "The Moist Maker"
+    },
+
+    {
+        questions: "Who did Ross talk to when he couldn't get his leather pants back on?",
+        answers: {
+            a: "Chandler",
+            b: "Phoebe",
+            c: "Monica",
+            d: "Joey",
+        },
+        correctAnswer: "Joey"
+    },
+
+    {
+        questions: "Chandler's Mother writes what kinds of novels?",
+        answers: {
+            a: "Fiction",
+            b: "Erotic",
+            c: "Mystery",
+            d: "Fantasy",
+        },
+        correctAnswer: "Erotic"
+    },
+    {
+        questions: "What is Phoebe's twin sister's name?",
+        answers: {
+            a: "Lisa",
+            b: "Emily",
+            c: "Dr. Phalange",
+            d: "Ursula",
+        },
+        correctAnswer: "Ursula"
+    },
+
+    {
+        questions: "What is kept in Monica's secret closet?",
+        answers: {
+            a: "Crap that didnt fit anywhere else",
+            b: "Collection of her workout tapes",
+            c: "Things from given to her by her ex-boyfiends",
+            d: "Nothing",
+        },
+        correctAnswer: "Crap that didnt fit anywhere else"
+    },
+    {
+        questions: "What is the name of the coffee house",
+        answers: {
+            a: "Coffee Perk",
+            b: "Park Perk",
+            c: "Left Perk",
+            d: "Central Perk",
+        },
+        correctAnswer: "Central Perk"
+    },
+
+
+
+
+
+
+
+]
